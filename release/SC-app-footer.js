71be1af5a67ea11ce9491d2658954f1f2d277ded
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:21:26 +0000

    initial commit

---
+++ release/SC-app-footer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-footer
+ * @description
+ *
+ * Provides the app with the ability to display footer content and features
+ */
+angular
+  .module('SC-app-footer');
